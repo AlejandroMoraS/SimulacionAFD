13c5ff36b2986e2ddd35aec2610a40cec80bb514
alexmora95 <alex-mora95@hotmail.com>
2018-03-01 16:58:02 -0600

    pequeños cambios

--- Simulacion.java
+++ Simulacion.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 
 public class Simulacion {
 
-    public static int estado(char []alfabeto, char simbolo) {
+    public static int estado(char []alfabeto, char caracterCadena) {
         for( int i = 0; i < alfabeto.length; i++ ) {
-            if( alfabeto[i] == simbolo )
+            if( alfabeto[i] == caracterCadena )
                 return i; }  return -1; }
 
+
     public static void main(String[] args) {
-        String scanString; StringBuilder concatenar = new StringBuilder(); String entrada = ""; char []alfabeto; int estadoInicial = -1; int []estadoFinal; int [][] transicion; int n=0;
-        try(Scanner scan = new Scanner(new File("D:\\Escritorio- Data\\automata.txt")))  { //Se busca el archivo en el escritorio
+        String scanString; StringBuilder imprimirCadena = new StringBuilder();  String entrada = ""; char []alfabeto; int estadoInicial = -1; int []estadoFinal; int [][] transicion; int n=0;
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------
+        try(
+                Scanner scan = new Scanner(new File("D:\\Escritorio- Data\\automata.txt")))  { //Se busca el archivo en el escritorio
             entrada = scan.nextLine(); //Se captura la primera linea del archivo (La cadena)
             scanString = scan.nextLine();
             alfabeto = new char[scanString.split(";").length]; //Se toma el alfabeto dividido con punto y coma usando .split
 
-            do{                                                       //Se hace un loop para tomar los simbolos del alfabeto
-                alfabeto[n] = scanString.split(";")[n].charAt(0); //Se itera el arreglo para guardar lo que este separado por ";"
+            do{
+                alfabeto[n] = scanString.split(";")[n].charAt(0); //Se itera el arreglo para guardar el alfabeto separado por ;
                 n++;
-            }while(n<scanString.split(";").length);
+            }while
+                    (n<scanString.split(";").length);
 
             estadoInicial = Integer.parseInt(scan.nextLine());  //Se escanea la siguiente linea que es el estado inicial
             scanString = scan.nextLine();                       //Se guarda en scanString para despues pasarlo al estado final
@@ -26,41 +31,49 @@ public class Simulacion {
             n=0;//reinicia n
 
             do {
-                estadoFinal[n] = Integer.parseInt(scanString.split(";")[n]); //Se guarda el estado final
+                estadoFinal[n] = Integer.parseInt(scanString.split(";")[n]); //Se guardan los estados finales separados por ;
                 n++;
-            }while (n<scanString.split(";").length); //se escanea la longitud en el archivo.txt
+            }while
+                    (n<scanString.split(";").length); //Condición para cubrir todos los estados finales
 
-            scanString = "";
-            do {            //Se hace un loop para obtener las transiciones
-                scanString += scan.nextLine()+"\n"; //Se escanea la siguiente linea
-            }while(scan.hasNextLine());
-            transicion = new int[scanString.split("\n").length][alfabeto.length]; //Se guarda la cadena del alfabeto
+            scanString = ""; //Se vacía el scanner de apoyo
+            do {
+                scanString += scan.nextLine()+"\n"; //Se guardan lo que queda del archivo, (transiciones)
+            }while(scan.hasNextLine()); //se siguen guardando mientras haya más lineas
+
+            transicion = new int[scanString.split("\n").length][alfabeto.length]; //Se reservan los espacios de transición
             for(int i=0; i<scanString.split("\n").length; i++) {
                 for(int h=0; h<alfabeto.length; h++) {
                     transicion[i][h] = Integer.parseInt(String.valueOf(scanString.split("\n")[i].split(";")[h].charAt(0))); }
+                //Se llena la matriz de un lado a los que van con 0(a) y del otro a los que van con 1(b)
             }
+            //-------------------------------------------------------------------------------------------------------------------------------------------
             System.out.printf("%s\nSecuencia de estados: %s\n",
                     IteracionCadena(entrada.toCharArray(),
                             alfabeto,
                             estadoInicial,
                             estadoFinal,
                             transicion,
-                            concatenar)?
-                            "Aceptada":"Rechazada",concatenar.toString()); //Se imprime la cadena como se pide
+                            imprimirCadena)?
+                            "Aceptada":"Denegada",
+                    imprimirCadena.toString()); //Se imprime la cadena como se pide
+
         }
-        catch(Exception ex) { //En caso de tener problemas con el archivo se imprimira un error
-            System.out.println("Error: "
-                    +ex.getMessage()); }
+        catch(Exception ex) { //En caso de no encontrar el archivo imprimira error
+            System.out.println("Error: El archivo no ha sido encontrado"); }
     }
 
     public static boolean IteracionCadena(char []palabra, char []alfabeto, int estadoInicial, int []estadoFinal, int [][] transicion, StringBuilder reccorido) {
         boolean regresa = false;
         int estadoActual = estadoInicial;           //Comienza en el estado inicial
         reccorido.append(String.format("%d/",estadoActual)); //guarda el estado inicial en el recorrido
-        for(char car : palabra) {
-            estadoActual = transicion[estadoActual][estado(alfabeto,car)]; //itera los estados para guardarlos en la siguiente instruccion llando a la funcion estado
-            reccorido.append(String.format("%d/",estadoActual)); }          //hace un toString de los estados
+        for(char caracterCadena : palabra) {
+            estadoActual = transicion[estadoActual][estado(alfabeto,caracterCadena)]; //itera la cadena llamando a la funcion estado
+            reccorido.append(String.format("%d/",estadoActual)); }          //Se concatena el recorrido de los estados
         for(int estadofinal : estadoFinal) {
-            regresa |= estadofinal == estadoActual; }return regresa;             //Se guarda el estado ginal
+            regresa |= estadofinal == estadoActual; }return regresa;   //si el estado final es igual al actual la cadena se acepta
     }
+
+
 }
+
